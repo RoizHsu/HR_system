9530d39d1a08c20100ccd92210c13afb36834a16
RoizHsu <qqa123456408@gmail.com>
2026-05-14 16:46:22 +0800

    add docker前後端資料且compose up 使用

--- HR_web/vite.config.js
+++ HR_web/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',// 監聽所有網絡介面 讓vite在所有網絡接口上監聽，這樣其他設備就可以通過IP地址訪問你的開發服務器
+    port: 5173,
+  },
 })
